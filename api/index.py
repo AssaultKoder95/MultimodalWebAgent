6a6ef837b0778f2659015a8bde39396fc6e2e473
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2024-05-16 22:16:03 +0530

    chore: update python api handler

--- api/index.py
+++ api/index.py
@@ -4,8 +4,9 @@ from src.oai_agent.oai_agent import main
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        main()
+        # This is the function that gets the data from the Open API
+        # main()
         self.send_response(200)
-        # self.send_header('Content-type','text/plain')
-        # self.end_headers()
-        # self.wfile.write('Test Application For Chima'.encode('utf-8'))
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Open API quota is over - please try later'.encode('utf-8'))
