5064b75b5e3f6958f36922e3209fd9ce5cb52935
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2024-05-16 14:28:25 +0530

    chore: try running webagent directly

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
 from http.server import BaseHTTPRequestHandler
- 
+from 'src/oai_agent' import main
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        main()
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Test Application For Chima'.encode('utf-8'))
+        # self.send_header('Content-type','text/plain')
+        # self.end_headers()
+        # self.wfile.write('Test Application For Chima'.encode('utf-8'))
